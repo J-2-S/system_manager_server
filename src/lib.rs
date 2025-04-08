6e4b0724bf07ed78bdee1b5d0fd48b8a10f4e519
Joshua <RanchMonster06@outlook.com>
2025-04-07 16:44:27 -1000

    I changed in to work as a exe and a lib so the api functions can be accessed

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub mod handlers;
+pub mod auth;
+pub mod update_manager;
+pub mod settings;
+pub mod server;
+
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 use std::{os::unix::process::CommandExt, process::{Command, Stdio}};
-mod handlers;
-mod auth;
-mod update_manager;
-mod settings;
-mod server;
+use system_manager_server::*;
 use libc::{setgid, setuid};
 use users::os::unix::UserExt;
 #[tokio::main]
 async fn main() {
-
 }
